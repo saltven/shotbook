e2cec2267755b46d1e6ac2d58ccf993d6306009a
saltven <39688612+saltven@users.noreply.github.com>
2021-01-21 18:25:41 +0800

    Add files via upload

---
+++ php/requestform-process.php
@@ -0,0 +1,67 @@
+<?php
+$errorMSG = "";
+
+if (empty($_POST["name"])) {
+    $errorMSG = "Name is required ";
+} else {
+    $name = $_POST["name"];
+}
+
+if (empty($_POST["email"])) {
+    $errorMSG = "Email is required ";
+} else {
+    $email = $_POST["email"];
+}
+
+if (empty($_POST["phone"])) {
+    $errorMSG = "Phone is required ";
+} else {
+    $phone = $_POST["phone"];
+}
+
+if (empty($_POST["select"])) {
+    $errorMSG = "Select is required ";
+} else {
+    $select = $_POST["select"];
+}
+
+if (empty($_POST["terms"])) {
+    $errorMSG = "Terms is required ";
+} else {
+    $terms = $_POST["terms"];
+}
+
+$EmailTo = "info@shotbook.app";
+$Subject = "You've got mail! Partner Enquiry";
+
+// prepare email body text
+$Body = "Hi Shotbook,\n\nI want to register my interest to become your partner:\n\n";
+$Body .= "Name: ";
+$Body .= $name;
+$Body .= "\n";
+$Body .= "Email: ";
+$Body .= $email;
+$Body .= "\n";
+$Body .= "Phone: ";
+$Body .= $phone;
+$Body .= "\n";
+$Body .= "Interested in: ";
+$Body .= $select;
+$Body .= "\n";
+$Body .= "Terms: ";
+$Body .= $terms;
+$Body .= "\n";
+
+// send email
+$success = mail($EmailTo, $Subject, $Body, "From:".$email);
+// redirect to success page
+if ($success && $errorMSG == ""){
+   echo "success";
+}else{
+    if($errorMSG == ""){
+        echo "Something went wrong :(";
+    } else {
+        echo $errorMSG;
+    }
+}
+?>
